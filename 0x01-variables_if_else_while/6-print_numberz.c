419df35cc10955b1a49cd64136cebc1bb09b9177
samuel isong <samuelbrownie37@gmail.com>
2022-10-14 16:30:31 -0700

    task 6 again

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,20 +7,10 @@
  */
 int main(void)
 {
-	unsigned char a = '0';
-	int i;
+	int a = 0;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(a);
-		a++;
-	}
-	a = '1';
-	for (i = 0; i < 6; i++)
-	{
+	for (; a < 10 ; a++)
 		putchar('0' + a);
-		a++;
-	}
 	putchar('\n');
 	return (0);
 }
