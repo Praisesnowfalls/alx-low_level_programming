a4875a4d9ff8991502822618cd4efe1a7f6a6504
samuel isong <samuelbrownie37@gmail.com>
2022-10-19 05:28:53 -0700

    task repaired

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code.
+ * _islower - main function
  *
- * Return: Always 0.
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ *
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
 }
